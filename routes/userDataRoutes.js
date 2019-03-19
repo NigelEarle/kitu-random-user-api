0685a76bdea3af39b15fb7224c5932c52b3048db
jhoun <jayhoun@gmail.com>
2019-03-18 14:09:11 -1000

    Add post user route

--- routes/userDataRoutes.js
+++ routes/userDataRoutes.js
@@ -10,19 +10,35 @@ const makeRequestArr = (numOfCalls, urlEndpoint) => {
 }
 
 router.get('/', async (req, res) => {
-  const [...response] = await axios.all(makeRequestArr(10, 'https://randomuser.me/api'));
-  [...response].map((data) => {
-    const { gender, name, location, email, cell } = data.data.results[0];
-    const userObj = {
-      gender: gender,
-      firstname: name.first,
-      city: location.city,
-      email: email,
-      cell: cell
-    }
-    db.userData.push(userObj);
-  })
-  return res.status(200).send(db.userData);
+  try {
+    const [...response] = await axios.all(makeRequestArr(1, 'https://randomuser.me/api'));
+    [...response].map((data) => {
+      const { gender, name, location, email, cell } = data.data.results[0];
+      const userObj = {
+        gender: gender,
+        firstname: name.first,
+        city: location.city,
+        email: email,
+        cell: cell
+      }
+      db.userData.push(userObj);
+    })
+    return res.status(200).send(db.userData);
+  } catch(err) {
+    console.log('err', err);
+    return res.status(404).send({message: 'Users not found'});
+  }
+
+})
+
+router.post('/', (req, res) => {
+  try {
+    db.userData.push(req.body);
+    return res.status(400).send({message: 'User successfully created!'});
+  } catch(err) {
+    console.log('err', err);
+    return res.status(404).send('title required');
+  }
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const userDataRoutes = require('./routes/userDataRoutes');
 const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/users', userDataRoutes);
 
